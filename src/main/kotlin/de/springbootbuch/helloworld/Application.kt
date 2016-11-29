e9cc07ee4f9285016850133939ae1f69656a53de
Michael J. Simons <michael@simons.ac>
2016-11-29 18:20:21 +0100

    Create helloworld_kotlin

---
+++ src/main/kotlin/de/springbootbuch/helloworld/Application.kt
@@ -0,0 +1,22 @@
+package de.springbootbuch.helloworld
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+open class Application {
+    @RestController
+    @RequestMapping(value = "/hello")
+    class HelloWorldController {
+        @GetMapping
+        fun helloWorld(@RequestParam name: String) = "Hello, " + name + "\n"
+    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
---
+++ src/test/kotlin/de/springbootbuch/helloworld/ApplicationTest.kt
@@ -0,0 +1,26 @@
+package de.springbootbuch.helloworld
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@RunWith(SpringRunner::class)
+@WebMvcTest
+class ApplicationTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test fun helloWorldShouldWork() {
+        mockMvc
+            .perform(get("/hello").param("name", "World"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Hello, World\n"))
+    }
+}
