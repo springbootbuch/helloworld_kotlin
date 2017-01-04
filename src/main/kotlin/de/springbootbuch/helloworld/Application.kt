462c052b66ade160ac89272618d3f5b639b689fa
Tim Riemer <tim.riemer@gmx.de>
2017-01-04 19:52:47 +0100

    Add kotlin-spring plugin and remove open keyword (#1)

--- src/main/kotlin/de/springbootbuch/helloworld/Application.kt
+++ src/main/kotlin/de/springbootbuch/helloworld/Application.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-open class Application {
+class Application {
     @RestController
     @RequestMapping(value = "/hello")
     class HelloWorldController {
