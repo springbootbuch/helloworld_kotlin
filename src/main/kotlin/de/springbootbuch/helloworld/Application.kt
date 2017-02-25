373ea866eb00c92c9e0941c8f7776a555cc69c74
Michael J. Simons <michael@simons.ac>
2017-02-25 18:52:00 +0100

    Fix layout for book

--- src/main/kotlin/de/springbootbuch/helloworld/Application.kt
+++ src/main/kotlin/de/springbootbuch/helloworld/Application.kt
@@ -3,16 +3,14 @@ package de.springbootbuch.helloworld
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class Application {
     @RestController
-    @RequestMapping(value = "/hello")
     class HelloWorldController {
-        @GetMapping
+        @GetMapping(value = "/hello")
         fun helloWorld(@RequestParam name: String) = "Hello, $name\n"
     }
 }
